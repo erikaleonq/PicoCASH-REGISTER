8f6fe8cd3441dedc1f2e51afecf7844b9be84105
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2023-12-09 23:17:27 -0500

    Receptor como modulo

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ reader = MFRC522(spi_id=0, sck=2, miso=4, mosi=3, cs=1, rst=0)
 # Configuración de la comunicación UART para enviar
 uart_send = UART(0, baudrate=1000000, tx=12, rx=13 )  # Ajusta los pines tx y rx según tu configuración
 
-# Configuración de la comunicación UART para recibir
-uart_receive = UART(1, baudrate=1000000, tx=8, rx=9)  # Ajusta los pines tx y rx según tu configuración
-
 print("Bring TAG closer...")
 print("")
 
@@ -45,11 +42,7 @@ while True:
             print("CARD ID:", card_id)
 
             # Envía el ID por UART
-            uart_send.write("CARD ID: {}\n".format(card_id))
+            uart_send.write("{}".format(card_id))
 
-            # Espera a recibir datos por UART
-            if uart_receive.any():
-                received_data = uart_receive.read()
-                print("Received:", received_data.decode("utf-8"))
 
     utime.sleep_ms(500)
--- receptor.c
+++ receptor.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/uart.h"
+#include "receptor.h"
 
-#define UART_ID uart1
-#define BAUD_RATE 1000000
-#define RX_PIN 5
+char received_message[5];
+int received_message_length = 0;
 
 void clear_uart_buffer(uart_inst_t *uart) {
     while (uart_is_readable(uart)) {
@@ -12,17 +9,15 @@ void clear_uart_buffer(uart_inst_t *uart) {
     }
 }
 
-int main() {
-    stdio_init_all();
+void init_uart(){
 
     uart_init(UART_ID, BAUD_RATE);
     gpio_set_function(RX_PIN, GPIO_FUNC_UART);
 
-    char received_message[256];
-    int received_message_length = 0;
+}
 
-    while (1) {
-        if (uart_is_readable(UART_ID)) {
+void recive_tag(){
+     if (uart_is_readable(UART_ID)) {
             char received_char = uart_getc(UART_ID);
             sleep_ms(1);
 
@@ -36,7 +31,4 @@ int main() {
                 received_message[0] = '\0';  // Reiniciar el mensaje
                 received_message_length = 0;
         }
-    }
-
-    return 0;
 }
